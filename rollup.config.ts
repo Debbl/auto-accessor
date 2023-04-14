e35332fe1a07e202c394b69dca55da43bb7c60e1
Debbl <me@aiwan.run>
2023-04-14 13:23:21 +0800

    fix: cjs

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   input: "./src/main.ts",
   output: {
     format: "cjs",
-    file: "./dist/index.js",
+    file: "./dist/index.cjs",
   },
   plugins: [nodeResolve(), typescript()],
   external: ["@actions/core"] 
