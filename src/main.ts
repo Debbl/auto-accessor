b985bfd88cce15058bb28260da1ee148ab6d13d5
Debbl <me@aiwan.run>
2023-04-14 13:42:43 +0800

    chore: test

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import * as core from "@actions/core";
+import fetch from "node-fetch";
 
 async function run(): Promise<void> {
   try {
     const url: string = core.getInput("url");
     core.info(url);
+    const response = await fetch(url);
+    const text = await response.text();
+    core.info("-------" + text);
     core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
